18e107fde37b4d61ef909f4a6b362e8d8ee16c97
david stefan <296599075+davidstefanciprian-afk@users.noreply.github.com>
2026-08-15 18:13:03 +0300

    Finish rock-paper-scissors project
    did not commit as often as i should've - have to improve on this
    
    Changes to be committed:
    	modified:   README.md
    	new file:   index.html
    	new file:   javascript.js
    	new file:   style.css

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Im trying to make the rock-papper-scissors project
 
 I wonder how many days its gonna take me, wish me luck 
+
+After the 4th day of beeing stuck at step 6 i caved in an searched other peoples solutions, CREDIT to user 0livana. 
+
+Forgot to git clone repos *facepalm* wondered why i was getting "fatal: not a git repository (or any of the parent directories): .git"...
+
+
+
---
+++ javascript.js
@@ -0,0 +1,60 @@
+/* hello world to check if .js is properly linked*/
+
+console.log('hello world') 
+
+/*computer choice function with math.random method and math.floor for rounding floating-point number*/
+
+function getComputerChoice() {
+    let number = Math.floor(Math.random()*10);
+    if(number <=3 ) {
+      return 'rock'
+    } else if (number <= 6) {
+      return 'paper'
+    } else {return 'scissors'}
+}
+
+/*human choice function with prompt method*/
+
+function getHumanChoice() {
+    return prompt('Choose rock, paper or scissors')
+}
+
+/*play round function wrapped in play game function, 4 days lost on this figuring a solution to this btw...credited the user for the solution in the README*/
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound (humanChoice, computerChoice) {
+    
+    humanChoice = humanChoice.toLowerCase();
+
+    if(humanChoice === computerChoice) {
+        return console.log(`It's a tie! You both picked ${humanChoice}`)
+    } else if 
+        ((humanChoice == 'rock' && computerChoice == 'scissors') ||
+        (humanChoice == 'paper' && computerChoice == 'rock') ||
+        (humanChoice == 'scissors' && computerChoice == 'paper')) {
+            humanScore++; return console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+    } else {
+        computerScore++; return console.log(`You loose! ${computerChoice} beats ${humanChoice}`)}
+    }
+  
+
+
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection)
+  }
+
+  
+  console.log(`Your score is ${humanScore}`)
+  console.log(`Computer score is ${computerScore}`)
+
+}
+
+playGame()
+
+/*cleaned up code for better readability*/
+
