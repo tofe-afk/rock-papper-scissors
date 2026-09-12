54f67418f0d737482ca6235da74f02815445e40b
david stefan <296599075+davidstefanciprian-afk@users.noreply.github.com>
2026-09-12 19:50:47 +0300

    remove logic that plays 5 rounds

--- javascript.js
+++ javascript.js
@@ -42,11 +42,11 @@ function playGame() {
   
 
 
-  for (let i = 0; i < 5; i++) {
+  /*for (let i = 0; i < 5; i++) {*/
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
     playRound(humanSelection, computerSelection)
-  }
+}
 
   
   function getTotalScore (x, z) {
