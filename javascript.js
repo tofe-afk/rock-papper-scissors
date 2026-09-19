b4eabe4bbac7a82333690b9ff0c5468e4abf8ab8
david stefan <296599075+davidstefanciprian-afk@users.noreply.github.com>
2026-09-19 18:20:57 +0300

    finish 90% of project, add comment for last 10%

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ let button = document.querySelector('#buttons')
 let rockBtn = document.querySelector('.rock')
 let paperBtn= document.querySelector('.paper')
 let scissorsBtn = document.querySelector('.scissors')
-let currentDiv = document.getElementById('results')
+let currentDiv = document.querySelector('#results')
 
 let humanChoice
 
@@ -42,44 +42,47 @@ function getComputerChoice() {
 
 function playGame() {
 
-
-
+ 
+ 
+  
   function playRound (humanChoices, computerChoice) {
 
 
     function createTextTie() {
-      const newDiv = document.createElement('div')
+      const newDiv = document.createElement('p')
       const message= document.createTextNode(`It's a tie! You both picked ${humanChoice}`)
       newDiv.appendChild(message)
       currentDiv = document.body.appendChild(newDiv, message)
     }
 
     function createTextWin() {
-      const newDiv = document.createElement('div')
+      const newDiv = document.createElement('p')
       const message = document.createTextNode(`You win! ${humanChoices} beats ${computerChoice}`)
       newDiv.appendChild(message)
       currentDiv = document.body.appendChild(newDiv, message)
     }
 
     function createTextLose() {
-      const newDiv = document.createElement('div')
+      const newDiv = document.createElement('p')
       const message = document.createTextNode(`You loose! ${computerChoice} beats ${humanChoices}`)
       newDiv.appendChild(message)
       currentDiv = document.body.appendChild(newDiv, message)
     }
 
+   
+
       humanChoices=humanChoice
 
     if(humanChoices === computerChoice) {
-      createTextTie()
+      createTextTie();
        // return console.log(`It's a tie! You both picked ${humanChoices}`)
     } else if 
         ((humanChoices == 'rock' && computerChoice == 'scissors') ||
         (humanChoices == 'paper' && computerChoice == 'rock') ||
         (humanChoices == 'scissors' && computerChoice == 'paper')) {
-            humanScore++; createTextWin()//return console.log(`You win! ${humanChoices} beats ${computerChoice}`)
+            humanScore++; createTextWin(); //return console.log(`You win! ${humanChoices} beats ${computerChoice}`)
     } else {
-        computerScore++; createTextLose()//return console.log(`You loose! ${computerChoice} beats ${humanChoices}`)
+        computerScore++; createTextLose(); //return console.log(`You loose! ${computerChoice} beats ${humanChoices}`)
         }
     
       }
@@ -96,10 +99,13 @@ function playGame() {
       x=humanScore
       z=computerScore
       
-      if (x==5 && x>z) {return console.log('You win against computer! Nice')}
-      else if (z==5) {return console.log('Computer beat you AHAH!!!')}
+      if (x==5 && x>z) { alert('You win against computer! Nice'); }
+      else if (z==5) { alert('Computer beat you AHAH!!!'); }
+
     }
     
+
+
 
   console.log(`Your score is ${humanScore}`)
   console.log(`Computer score is ${computerScore}`)
@@ -109,10 +115,14 @@ function playGame() {
   getTotalScore(humanScore, computerScore)
 
 
-  if(humanScore===5) {humanScore=0; computerScore=0;}
-  else if (computerScore ===5 ) {humanScore=0; computerScore=0;}
+  if(humanScore===5) {humanScore=0; computerScore=0; location.reload();}
+  else if (computerScore ===5 ) {humanScore=0; computerScore=0; location.reload();}
 
     
   }
 
 button.addEventListener('click', playGame)
+
+
+/*last thing to display players score on page, going to stop, spent enough days on the project
+should be easy enough, create div with text in html and create function to change score with embedded
