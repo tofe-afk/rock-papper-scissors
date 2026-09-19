56b91c5e8ef26acb7fe2067cc7d9f154372d2879
david stefan <296599075+davidstefanciprian-afk@users.noreply.github.com>
2026-09-19 17:32:07 +0300

    announce winner with alert

--- javascript.js
+++ javascript.js
@@ -4,20 +4,26 @@ let button = document.querySelector('#buttons')
 let rockBtn = document.querySelector('.rock')
 let paperBtn= document.querySelector('.paper')
 let scissorsBtn = document.querySelector('.scissors')
-
+let currentDiv = document.getElementById('results')
 
 let humanChoice
 
 
-  function sdf() {return humanChoice='rock'}
-  function dfs() {return humanChoice='paper'}
-  function sfd() {return humanChoice='scissors'}
+function sdf() {return humanChoice='rock'}
+function dfs() {return humanChoice='paper'}
+function sfd() {return humanChoice='scissors'}
 
 
-   rockBtn.addEventListener("click", sdf)
-   paperBtn.addEventListener("click", dfs)
-   scissorsBtn.addEventListener("click", sfd)
+rockBtn.addEventListener("click", sdf)
+paperBtn.addEventListener("click", dfs)
+scissorsBtn.addEventListener("click", sfd)
 
+/*function createTextRock() {
+  const newDiv = document.createElement('div')
+  const message= document.createTextNode(`It's a tie! You both picked ${humanChoice}`)
+  newDiv.appendChild(message)
+  currentDiv = document.body.insertBefore(newDiv, currentDiv)
+}*/
 
 
 function getComputerChoice() {
@@ -36,21 +42,45 @@ function getComputerChoice() {
 
 function playGame() {
 
-  
-  
+
 
   function playRound (humanChoices, computerChoice) {
+
+
+    function createTextTie() {
+      const newDiv = document.createElement('div')
+      const message= document.createTextNode(`It's a tie! You both picked ${humanChoice}`)
+      newDiv.appendChild(message)
+      currentDiv = document.body.appendChild(newDiv, message)
+    }
+
+    function createTextWin() {
+      const newDiv = document.createElement('div')
+      const message = document.createTextNode(`You win! ${humanChoices} beats ${computerChoice}`)
+      newDiv.appendChild(message)
+      currentDiv = document.body.appendChild(newDiv, message)
+    }
+
+    function createTextLose() {
+      const newDiv = document.createElement('div')
+      const message = document.createTextNode(`You loose! ${computerChoice} beats ${humanChoices}`)
+      newDiv.appendChild(message)
+      currentDiv = document.body.appendChild(newDiv, message)
+    }
+
       humanChoices=humanChoice
 
     if(humanChoices === computerChoice) {
-        return console.log(`It's a tie! You both picked ${humanChoices}`)
+      createTextTie()
+       // return console.log(`It's a tie! You both picked ${humanChoices}`)
     } else if 
         ((humanChoices == 'rock' && computerChoice == 'scissors') ||
         (humanChoices == 'paper' && computerChoice == 'rock') ||
         (humanChoices == 'scissors' && computerChoice == 'paper')) {
-            humanScore++; return console.log(`You win! ${humanChoices} beats ${computerChoice}`)
+            humanScore++; createTextWin()//return console.log(`You win! ${humanChoices} beats ${computerChoice}`)
     } else {
-        computerScore++; return console.log(`You loose! ${computerChoice} beats ${humanChoices}`)}
+        computerScore++; createTextLose()//return console.log(`You loose! ${computerChoice} beats ${humanChoices}`)
+        }
     
       }
 
@@ -61,14 +91,13 @@ function playGame() {
     playRound(humanChoice, computerSelection)
 
   
-  function getTotalScore (x, z) {
+    function getTotalScore (x, z) {
 
-    x=humanScore
-    z=computerScore
-    
-    if (x==5 && x>z) {return console.log('You win against computer! Nice')}
-    else if (z==5) {return console.log('Computer beat you AHAH!!!')}
-    //else if (x==5 && z==5) {return console.log('Well...thats a tie, boooring')}
+      x=humanScore
+      z=computerScore
+      
+      if (x==5 && x>z) {return console.log('You win against computer! Nice')}
+      else if (z==5) {return console.log('Computer beat you AHAH!!!')}
     }
     
 
@@ -80,8 +109,9 @@ function playGame() {
   getTotalScore(humanScore, computerScore)
 
 
-  if(humanScore===5) {humanScore=0; computerScore=0}
-  else if (computerScore ===5 ) {humanScore=0; computerScore=0}
+  if(humanScore===5) {humanScore=0; computerScore=0;}
+  else if (computerScore ===5 ) {humanScore=0; computerScore=0;}
+
     
   }
 
